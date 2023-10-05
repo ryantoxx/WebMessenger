d4c8f753dfe5e5036166f565460afc33fdc2ed9d
zly-VON <113551094+zly-VON@users.noreply.github.com>
2023-10-05 22:19:09 +0300

    (f) add: login

---
+++ frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,24 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Link, Stack, Typography } from "@mui/material";
+import React from "react";
+import LoginForm from "../../sections/auth/LoginForm";
+
+const Login = () => {
+  return (
+    <>
+      <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
+        <Typography variant="h3">Login</Typography>
+        <Stack direction="row" spacing={0.5}>
+          <Typography variant="body2">New User?</Typography>
+          <Link to="/auth/register" component={RouterLink} variant="subtitle2">
+            Create an account
+          </Link>
+        </Stack>
+        {/* Login Form */}
+        <LoginForm />
+      </Stack>
+    </>
+  );
+};
+
+export default Login;
