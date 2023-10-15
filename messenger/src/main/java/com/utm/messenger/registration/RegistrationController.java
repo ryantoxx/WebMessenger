bbb87e69c79f2354f9149eb1f92e8186cd9caa1c
ryantoxx <114425150+ryantoxx@users.noreply.github.com>
2023-10-15 21:08:08 +0300

    (b) add: connecting to the db

--- messenger/src/main/java/com/utm/messenger/registration/RegistrationController.java
+++ messenger/src/main/java/com/utm/messenger/registration/RegistrationController.java
@@ -2,6 +2,8 @@ package com.utm.messenger.registration;
 
 import com.utm.messenger.event.RegistrationCompleteEvent;
 import com.utm.messenger.exception.UserAlreadyExistsException;
+import com.utm.messenger.registration.token.VerificationToken;
+import com.utm.messenger.registration.token.VerificationTokenRepository;
 import com.utm.messenger.user.User;
 
 import com.utm.messenger.event.RegistrationCompleteEvent;
@@ -11,9 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,16 +22,29 @@ public class RegistrationController {
 
     private final UserService userService;
     private final ApplicationEventPublisher publisher;
+    private final VerificationTokenRepository tokenRepository;
 
     @PostMapping
-    public String registerUser(RegistrationRequest registrationRequest, final HttpServletRequest request) {
+    public String registerUser(@RequestBody RegistrationRequest registrationRequest, final HttpServletRequest request) {
         User user = userService.registerUser(registrationRequest);
         // publish registration event
         publisher.publishEvent(new RegistrationCompleteEvent(user, applicationUrl(request)));
         return "Successful. Check your email to complete your registration";
     }
 
+    @GetMapping("/verifyEmail")
+    public String verifyEmail(@RequestParam("token") String token) {
+        VerificationToken theToken = tokenRepository.findByToken(token);
+        if (theToken.getUser().isEnabled()) {
+            return "This account has already been verified, please log in";
+        }
+        String verificationResult = userService.validateToken(token);
+        if(verificationResult.equalsIgnoreCase("Valid")) {
+            return "Email verified successfully, now you can log in to your account.";
+        }
+        return "Invalid verification token";
 
+    }
 
     public String applicationUrl(HttpServletRequest request) {
         return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
--- messenger/src/main/java/com/utm/messenger/user/IUserService.java
+++ messenger/src/main/java/com/utm/messenger/user/IUserService.java
@@ -1,6 +1,7 @@
 package com.utm.messenger.user;
 
 import com.utm.messenger.registration.RegistrationRequest;
+import com.utm.messenger.registration.token.VerificationToken;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +12,6 @@ public interface IUserService {
     Optional<User> findByEmail(String email);
 
     void saveUserVerificationToken(User theUser, String token);
+
+    String validateToken(String theToken);
 }
