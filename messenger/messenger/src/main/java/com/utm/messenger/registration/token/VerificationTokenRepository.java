7754e1e42dedbd20bd150d4adce453b7ede0c6af
ryantoxx <114425150+ryantoxx@users.noreply.github.com>
2023-10-15 21:07:20 +0300

    Delete messenger/messenger directory

--- messenger/messenger/src/main/java/com/utm/messenger/registration/token/VerificationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.utm.messenger.registration.token;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
-    VerificationToken findByToken(String token);
-}
